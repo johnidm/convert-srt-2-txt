fdc197d5559884791c0324c466b1ddd692c3d4c8
Johni Marangon <johnimarangon@Johnis-MacBook-Air.local>
2024-11-21 22:05:03 -0300

    add file upload

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const content = ref('')
+
+async function handleFileChange(event) {
+  const file = event.target.files[0]
+
+  const text = await file.text()
+  content.value = toTxt(text)
+}
+
+function toTxt(text) {
+  return text.toLowerCase()
+}
+</script>
 
 <template>
   <header>Header</header>
 
-  <main>Main</main>
+  <main>
+    <div>
+      <input type="file" @change="handleFileChange" accept=".srt" />
+
+      <div v-if="content">
+        <h3>File Content:</h3>
+        <pre>{{ content }}</pre>
+      </div>
+    </div>
+  </main>
 
   <footer>Footer</footer>
 </template>
